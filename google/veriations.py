86e37b14f3277adba25328a73633e2966f3858f0
michalReich200 <86185028+michalReich200@users.noreply.github.com>
2021-07-21 23:49:19 +0300

    21/07/2021

---
+++ google/veriations.py
@@ -0,0 +1,10 @@
+def get_all_variations(sentence):
+    """
+
+    :param sentence: that we want to get all options to fix it.
+    :return:list of all the options to fix the sentence.
+    """
+
+    variations_list = []
+
+    return variations_list
